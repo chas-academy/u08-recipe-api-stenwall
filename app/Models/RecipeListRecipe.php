0fc542f982da311128bcb4c0483e496adfce5ecd
Karin Stenwall <karin.stenwall@gmail.com>
2021-08-07 21:55:30 +0200

    add: models

---
+++ app/Models/RecipeListRecipe.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RecipeListRecipe extends Model
+{
+    use HasFactory;
+}
